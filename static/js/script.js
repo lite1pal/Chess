842b36631772c1a99817760f4ca2b52ecab0c5ec
lite1pal <deniskatarasenko6@gmail.com>
2023-01-20 11:55:23 +0200

    pawn can't go on unavailable squares

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,20 @@
 let clickedFigure = "";
 let nameOfFigure = "";
 let isSomeFigureClicked = false;
-let availableMove = "";
+let availableMoveY = "";
+let availableMoveX = "";
+const charsInNumbers = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "g": 6,
+    "f": 7,
+    "h": 8
+}
 
 document.querySelector("#board").addEventListener("click", function (e) {
-
     // loops through squares to find taken ones
     const squaresWithFigures = [];
     const squaresWithoutFigures = [];
@@ -19,11 +29,21 @@ document.querySelector("#board").addEventListener("click", function (e) {
     }
 
     const movingOnAvailableSquares = function () {
-        if (e.target.id === availableMove) {
+        if (e.target.id === availableMoveY) {
             document.getElementById(clickedFigure).style.backgroundColor = "";
             document.getElementById(e.target.id).innerHTML = document.getElementById(clickedFigure).innerHTML;
             document.getElementById(clickedFigure).innerHTML = "";
-            document.getElementById(availableMove).style.backgroundColor = "";
+            document.getElementById(availableMoveY).style.backgroundColor = "";
+        }
+    }
+
+    const removesAvailableMoveWithFigureOnIt = function () {
+        if (squaresWithFigures.includes(availableMoveY)) {
+            availableMoveY = "";
+        }
+        else {
+            document.getElementById(availableMoveY).style.backgroundColor = "green";
+
         }
     }
 
@@ -32,7 +52,10 @@ document.querySelector("#board").addEventListener("click", function (e) {
         // removes highlighting from the previous figure if the clicked figure is different
         if (clickedFigure && clickedFigure !== e.target.parentNode) {
             document.getElementById(clickedFigure).style.backgroundColor = "";
-            document.getElementById(availableMove).style.backgroundColor = "";
+
+            if (availableMoveY) {
+                document.getElementById(availableMoveY).style.backgroundColor = "";
+            }
         }
 
         // highlights figure if it was clicked
@@ -42,17 +65,25 @@ document.querySelector("#board").addEventListener("click", function (e) {
 
         switch (e.target.id) {
             case "w-pawn":
-                availableMove = e.target.parentNode.id.slice(0, 2) + (parseInt(e.target.parentNode.id[2]) + 1).toString();
-                document.getElementById(availableMove).style.backgroundColor = "green";
-        }
+                availableMoveY = e.target.parentNode.id.slice(0, 2) + (parseInt(e.target.parentNode.id[2]) + 1).toString();
+                removesAvailableMoveWithFigureOnIt();
+                break;
+            case "b-pawn":
+                availableMoveY = e.target.parentNode.id.slice(0, 2) + (parseInt(e.target.parentNode.id[2]) - 1).toString();
+                removesAvailableMoveWithFigureOnIt();
+                break;
 
+        }
+        console.log(availableMoveY)
         isSomeFigureClicked = true;
     }
-
     // manages moving on the free squares
     else if (isSomeFigureClicked && e.target.id !== "board") {
         switch (nameOfFigure) {
             case "w-pawn":
+                movingOnAvailableSquares();
+                break;
+            case "b-pawn":
                 movingOnAvailableSquares();
                 break;
         }
